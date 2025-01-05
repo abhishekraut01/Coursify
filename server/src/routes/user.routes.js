ecb3c36894afe05b2fa6c65b7abae54cf1748ee4
Abhishek <Abhishekraut8697@gmail.com>
2025-01-05 21:39:40 +0530

    setting up user route

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,4 +1,37 @@
-import express from "express";
-const userRouter = express.Router()
+import express from 'express';
+const userRouter = express.Router();
 
-export default userRouter
+// POST routes for creating resources or performing actions
+userRouter.post('/signup', (req, res) => {
+  // Logic for signing up
+});
+
+userRouter.post('/login', (req, res) => {
+  // Logic for logging in
+});
+
+userRouter.post('/logout', (req, res) => {
+  // Logic for logging out
+});
+
+// GET route to fetch courses (reading data)
+userRouter.get('/courses', (req, res) => {
+  // Logic to get all courses
+});
+
+// POST route to create a new course (adding a resource)
+userRouter.post('/courses', (req, res) => {
+  // Logic for adding a new course
+});
+
+// PUT route for updating a course (modify an existing resource)
+userRouter.put('/courses/:id', (req, res) => {
+  // Logic for updating a course
+});
+
+// DELETE route for deleting a course (removing a resource)
+userRouter.delete('/courses/:id', (req, res) => {
+  // Logic for deleting a course
+});
+
+export default userRouter;
