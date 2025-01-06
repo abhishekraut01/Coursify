b2d99f0117b41548329e86146062514335080d55
Abhishek <Abhishekraut8697@gmail.com>
2025-01-06 16:05:20 +0530

    created login logout controller in user controller

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -55,12 +55,73 @@ export const userSignUp = async (req, res) => {
 
 
 export const userLogin = async (req, res) => {
-  // Logic for logging in
+  const validationResponse = loginSchema.safeParse(req.body);
+
+  if (!validationResponse.success) {
+    return res.status(400).json({
+      success: false,
+      message: 'Input is invalid, please try again.',
+      errors: validationResponse.error.errors,
+    });
+  }
+
+  const { username, password } = req.body;
+
+  try {
+    const isUserExist = await User.findOne({ username });
+    if (!isUserExist) {
+      return res.status(401).json({
+        success: false,
+        message: 'Invalid username or password.',
+      });
+    }
+
+    const validatePassword = await bcrypt.compare(
+      password,
+      isUserExist.password
+    );
+
+    if (!validatePassword) {
+      return res.status(401).json({
+        success: false,
+        message: 'Invalid username or password.',
+      });
+    }
+
+    const token = jwt.sign({ UserId: isUserExist._id }, process.env.JWT_KEY, {
+      expiresIn: '1d',
+    });
+
+    res
+      .status(200)
+      .cookie('jwt', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict',
+        maxAge: 24 * 60 * 60 * 1000,
+      })
+      .json({
+        success: true,
+        message: 'User logged in successfully.',
+      });
+  } catch (error) {
+    console.error('Error in Login controller:', error);
+    next(error);
+  }
 };
 
 
 export const userLogout = async (req, res) => {
-  // Logic for logging out
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'strict',
+    expires: new Date(0),
+  });
+
+  res.status(200).json({
+    message: 'Logged out successfully',
+  });
 };
 
 
